f1819919e2ae50133472fcc265fecfcad36a85a8
Matt Mason <matt@curdsandco.com>
2017-10-21 11:49:01 -0400

    initial

---
+++ revit_addin/AirflowDesigner/Command.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using System.Windows.Forms;
+
+namespace AirflowDesigner
+{
+    [Transaction(TransactionMode.Manual), Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+
+            }
+            catch (ApplicationException aex)
+            {
+                MessageBox.Show(aex.Message);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ revit_addin/AirflowDesigner/ExternalApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace AirflowDesigner
+{
+    public class ExternalApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
