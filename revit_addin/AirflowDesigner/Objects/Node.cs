4f8661cdb61ce73b98e9c532ab0f43f74706f2d0
Arif Hanif <ahanif@aeieng.com>
2017-10-21 12:47:20 -0400

    gitignore and node object

---
+++ revit_addin/AirflowDesigner/Objects/Node.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflowDesigner.Objects
+{
+    class Node
+    {
+        private static int COUNTER = 0;
+        public enum NodeTypeEnum { Vav, Shaft, Other };
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        public NodeTypeEnum NodeType { get; set; }
+
+        public XYZ Location { get; set; }
+
+        public Node()
+        {
+            COUNTER++;
+            Id = COUNTER;
+        }
+        public override string ToString()
+        {
+            return NodeType + ": " + Id + ": " + Name;
+        }
+    }
+}
