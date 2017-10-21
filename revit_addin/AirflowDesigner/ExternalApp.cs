2e007d0f38823c0fb1371a624512d1408ffc9762
Matt Mason <matt@curdsandco.com>
2017-10-21 11:59:15 -0400

    ui updates

--- revit_addin/AirflowDesigner/ExternalApp.cs
+++ revit_addin/AirflowDesigner/ExternalApp.cs
@@ -9,14 +9,31 @@ namespace AirflowDesigner
 {
     public class ExternalApp : IExternalApplication
     {
+        private Autodesk.Revit.UI.UIControlledApplication _app;
+
         public Result OnShutdown(UIControlledApplication application)
         {
+            _app = application;
+            buildUI();
             return Result.Succeeded;
         }
 
         public Result OnStartup(UIControlledApplication application)
         {
             return Result.Succeeded;
+        }
+
+        private void buildUI()
+        {
+            var panel = _app.CreateRibbonPanel(Tab.AddIns, "Airflow" + Environment.NewLine + "Designer");
+
+            var save = new PushButtonData("AirflowDesigner", "Airflow Designer", System.Reflection.Assembly.GetExecutingAssembly().Location, "AirflowDesigner.Command");
+            save.ToolTip = "Analyze the model for airflow, help make decisions";
+            save.LongDescription = "Look at possible shaft locations";
+            //save.LargeImage = getImage("LifeSaver.Images.lifesaver-32.png");
+            //save.Image = getImage("LifeSaver.Images.lifesaver-16.png");
+
+            panel.AddItem(save);
         }
     }
 }
