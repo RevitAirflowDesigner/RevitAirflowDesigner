81572563122c3b7352f52238cb5d41d6f5cf961e
Matt Mason <matt@curdsandco.com>
2017-10-21 21:05:23 -0400

    add results classes.

---
+++ revit_addin/AirflowDesigner/Objects/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflowDesigner.Objects
+{
+    public class Solution
+    {
+        public String Shaft { get; set; }
+        public double SheetMetal { get; set; }
+        public double Cost { get; set; }
+        public double StaticPressure { get; set; }
+
+        public List<Edge> Edges { get; set; }
+    }
+}
